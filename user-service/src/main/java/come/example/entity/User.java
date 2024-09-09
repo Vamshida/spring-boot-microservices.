065a413aa7f35c323b72780ecdf50a1d93b648a0
Vamshida <48756311+Vamshida@users.noreply.github.com>
2024-09-09 10:07:49 +0530

    Update User.java

--- user-service/src/main/java/come/example/entity/User.java
+++ user-service/src/main/java/come/example/entity/User.java
@@ -1,1 +1,14 @@
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
