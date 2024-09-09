7ce1374969ea21ada71dfbe86f156cd0fc053766
Vamshida <48756311+Vamshida@users.noreply.github.com>
2024-09-09 10:28:52 +0530

    Update Journal.java

--- journal-service/src/main/java/come/example/entity/Journal.java
+++ journal-service/src/main/java/come/example/entity/Journal.java
@@ -1,1 +1,11 @@
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Journal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String message;
+}
