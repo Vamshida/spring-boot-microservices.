19686d0617b5c171f373d7e8ece4b058ffd76497
Vamshida <48756311+Vamshida@users.noreply.github.com>
2024-09-09 10:28:28 +0530

    Update JournalController.java

--- journal-service/src/main/java/come/example/controller/JournalController.java
+++ journal-service/src/main/java/come/example/controller/JournalController.java
@@ -1,1 +1,11 @@
+@RestController
+@RequestMapping("/journals")
+public class JournalController {
+    @Autowired
+    private JournalService journalService;
 
+    @GetMapping
+    public ResponseEntity<List<Journal>> getJournals() {
+        return new ResponseEntity<>(journalService.getAllJournals(), HttpStatus.OK);
+    }
+}
