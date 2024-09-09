84b958f31b51b4c5d5c013636e02c143d8b91611
Vamshida <48756311+Vamshida@users.noreply.github.com>
2024-09-09 10:18:19 +0530

    Update UserDetailServiceImpl.java

--- user-service/src/main/java/come/example/security/UserDetailServiceImpl.java
+++ user-service/src/main/java/come/example/security/UserDetailServiceImpl.java
@@ -1,1 +1,17 @@
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    
+    @Autowired
+    private UserRepository userRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                AuthorityUtils.createAuthorityList(user.getRole().name()));
+    }
+}
