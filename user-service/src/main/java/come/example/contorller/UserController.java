686bbefa9502f7556767ce5209fb4dcc235d1337
Vamshida <48756311+Vamshida@users.noreply.github.com>
2024-09-09 10:07:15 +0530

    Update UserController.java

--- user-service/src/main/java/come/example/contorller/UserController.java
+++ user-service/src/main/java/come/example/contorller/UserController.java
@@ -1,1 +1,30 @@
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
 
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        user.setRole(Role.ROLE_USER);  // or set ROLE_ADMIN
+        return new ResponseEntity<>(userService.registerUser(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userService.getUser(id)
+                .map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return new ResponseEntity<>(userService.updateUser(id, user), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
