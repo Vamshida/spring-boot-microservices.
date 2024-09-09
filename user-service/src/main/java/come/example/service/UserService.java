4fe22648c79132cbcc09b601b2a7add8e37e4aa9
Vamshida <48756311+Vamshida@users.noreply.github.com>
2024-09-09 10:08:51 +0530

    Update UserService.java

--- user-service/src/main/java/come/example/service/UserService.java
+++ user-service/src/main/java/come/example/service/UserService.java
@@ -1,1 +1,28 @@
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));  // Encode password
+        kafkaTemplate.send("user-events", "User Registered: " + user.getUsername());
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUser(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+        return userRepository.save(updatedUser);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+        kafkaTemplate.send("user-events", "User Deleted: " + id);
+    }
+}
